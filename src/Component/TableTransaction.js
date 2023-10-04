b55447617e3c437cb23fb6b82ff73c16fc014f6f
AmitpaulCNS <paul.amit.cns@gmail.com>
2023-10-04 11:30:40 +0530

    update code

--- src/Component/TableTransaction.js
+++ src/Component/TableTransaction.js
@@ -306,13 +306,13 @@ const TableTransaction = ({ FilterData, purpose, page, handlePage, totalPage }) 
                     <span
                         className={`m-3 `}
                     >
-                        <button className={`btn btn-primary ${page === 1 ? 'disabled' : ''}`} onClick={() => { page > 1 && handlePage(page - 1) }}>Pre</button>
+                        <button className={`btn btn-primary rounded-pill ${page === 1 ? 'disabled' : ''}`} onClick={() => { page > 1 && handlePage(page - 1) }}>Pre</button>
                     </span>
                     <span className='fs-4'>{page}</span>
                     <span
                         className={`m-3 `}
                     >
-                        <button className={`btn btn-primary ${page === totalPage ? 'disabled' : ''}`} onClick={() => { handlePage(page + 1) }}
+                        <button className={`btn btn-primary rounded-pill ${page === totalPage ? 'disabled' : ''}`} onClick={() => { handlePage(page + 1) }}
                         >Next</button>
                     </span>
                     {/* jump to: */}
